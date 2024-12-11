09446969d9bf3c1e7f493728e0452a07d55ab79e
Sacit <sacidaktan1@hotmail.com>
2024-12-12 02:17:52 +0300

    The project

--- main.c
+++ main.c
@@ -1,45 +1,20 @@
-#include <SDL2/SDL.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "config.h"
+#include <unistd.h>
+
+// Declare the function defined in `audio_player.c`
+void playAudio(const char *filename);
 
 int main(void) {
-    if (SDL_Init(SDL_INIT_AUDIO) < 0) {
-        printf("SDL initialization failed: %s\n", SDL_GetError());
-        return 1;
-    }
-
-    // Construct the full path using the CMake-defined ASSETS_DIR
     char audioPath[1024];
     snprintf(audioPath, sizeof(audioPath), "%s/kof.wav", ASSETS_DIR);
 
-    SDL_AudioSpec wavSpec;
-    Uint32 wavLength;
-    Uint8 *wavBuffer;
+    printf("please wait for 2 seconds\n");
 
-    if (SDL_LoadWAV(audioPath, &wavSpec, &wavBuffer, &wavLength) == NULL) {
-        printf("Loading WAV file failed: %s\n", SDL_GetError());
-        SDL_Quit();
-        return 1;
-    }
+    sleep(2);
 
-    SDL_AudioDeviceID audioDevice = SDL_OpenAudioDevice(NULL, 0, &wavSpec, NULL, 0);
-    if (audioDevice == 0) {
-        printf("Failed to open audio device: %s\n", SDL_GetError());
-        SDL_FreeWAV(wavBuffer);
-        SDL_Quit();
-        return 1;
-    }
-
-    if (SDL_QueueAudio(audioDevice, wavBuffer, wavLength) < 0) {
-        printf("Failed to queue audio: %s\n", SDL_GetError());
-    }
-    SDL_PauseAudioDevice(audioDevice, 0);
-
-    SDL_Delay((wavLength * 1000) / wavSpec.freq);
-
-    SDL_CloseAudioDevice(audioDevice);
-    SDL_FreeWAV(wavBuffer);
-    SDL_Quit();
+    playAudio(audioPath);
 
     return 0;
 }
